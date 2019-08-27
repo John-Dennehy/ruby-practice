74f56c5f28fea727bd54ff15c640d3fea3e299c5
JDtheGeek <john.dennehy@gmail.com>
2019-08-27 13:56:05 +0100

    Update to roll 2d6 per player

--- game_of_dice.rb
+++ game_of_dice.rb
@@ -5,15 +5,19 @@ players = ["player1","player2"]
 
 while keep_playing
   draw = false  # reset
-  scores = []
-
-  puts "Rolling the dice!"
-
+  scores = []  # reset
+  dice = []  #reset
+  
   players.each { |player|
-    scores.push (rand(5).to_i + 1)
+    puts "#{player} is rolling!"
+    2.times {
+      dice.push (rand(5).to_i + 1)
+      puts "#{dice[-1]}"
+    } 
+    scores.push ( dice[-1] + dice[-2] )
   }
 
-  puts "Player 1 rolls #{scores[0]} and player two rolls #{scores[1]}"
+  puts "Player 1 scored #{scores[0]} and player two scored #{scores[1]}"
   draw = true if scores[0] == scores[1]
   puts "Player 1 wins!" if scores[0] > scores[1]
   puts "Player 2 wins!" if scores[1] > scores[0]
