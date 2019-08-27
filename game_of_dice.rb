0bb010f94793c56b976737290cce1ecc3ee2c78e
JDtheGeek <john.dennehy@gmail.com>
2019-08-27 14:43:15 +0100

    Updated game: Winner is first to 2 rounds

--- game_of_dice.rb
+++ game_of_dice.rb
@@ -1,29 +1,42 @@
-
 keep_playing = true
-
-players = ["player1","player2"]
+players = ["Player 1","Player 2"]
+p1_rounds = 0
+p2_rounds = 0
 
 while keep_playing
-  draw = false  # reset
   scores = []  # reset
   dice = []  #reset
   
   players.each { |player|
-    puts "#{player} is rolling!"
+    print "\n#{player} rolls: "
     2.times {
       dice.push (rand(5).to_i + 1)
-      puts "#{dice[-1]}"
+      print "#{dice[-1]} "
     } 
     scores.push ( dice[-1] + dice[-2] )
   }
 
-  puts "Player 1 scored #{scores[0]} and player two scored #{scores[1]}"
-  draw = true if scores[0] == scores[1]
-  puts "Player 1 wins!" if scores[0] > scores[1]
-  puts "Player 2 wins!" if scores[1] > scores[0]
-    
-  keep_playing = false unless draw
+  puts "\n\nPlayer 1 scored: #{scores[-2]} - Player two scored: #{scores[-1]}" 
   
+  if scores[-2] > scores[-1]
+    # player 1 wins
+    p1_rounds += 1
+    print "Player 1 has won this round"
+    keep_playing = false if p1_rounds >= 2
+    
+  elsif scores[-2] < scores[-1]
+    #player 2 wins
+    p2_rounds += 1
+    print "Player 2 has won this round"
+    keep_playing = false if p2_rounds >= 2
+
+  elsif scores[-2] == scores[-1]
+    # draw
+    puts "This round is a draw!"
+  end
+
+  print " and the game ( #{p1_rounds} Vs. #{p2_rounds} )\n" if keep_playing == false
+  puts "\n"
 end
 
 
