40294fdca101ac464edea445080812f9356f1d4a
JDtheGeek <john.dennehy@gmail.com>
2019-08-29 21:08:50 +0100

    ouput = number if neither fizz nor buzz. Now works on CLI

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,16 @@
 
 def fizzbuzz(number)
-  output = ""
-  output += "Fizz" if number % 3 == 0
-  output += "Buzz" if number % 5 == 0
-  return output
+  output = "" # start with blank output
+  output += "Fizz" if number % 3 == 0 # if divisable by 3 add Fizz to output
+  output += "Buzz" if number % 5 == 0 # if divisable by 5 add Buzz to output
+  output = number if output == "" # if output is still blank, make output the number 
+  return output # return output
+end
+
+# for testing
+while true
+  print "Enter a number (or 'stop'): "
+  input = gets.chomp # get number from user
+  break if input == "stop"
+  print " = #{fizzbuzz(input.to_i)}\n" # return output
 end
