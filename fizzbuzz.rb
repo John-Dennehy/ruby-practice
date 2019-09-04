556e7cba09fd460be6baa8e2bda1860e0e128223
JDtheGeek <john.dennehy@gmail.com>
2019-09-04 17:55:21 +0100

    refactored to replace hardcoded values/strings with key/value hash

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,23 @@
 
-def fizzbuzz(number)
-  output = "" # start with blank output
-  output += "Fizz" if number % 3 == 0 # if divisable by 3 add Fizz to output
-  output += "Buzz" if number % 5 == 0 # if divisable by 5 add Buzz to output
-  output = number if output == "" # if output is still blank, make output the number 
-  return output # return output
-end
+def fizzbuzz
+  range = 1..30
+  hash = {
+    3 => "Fizz",
+    5 => "Buzz"
+  }
 
-# # for testing method
-# print "Enter a number: "
-# until_number = gets.chomp.to_i # get number from user
-# (1..until_number).each do |number| # for each number in the range
-#   puts fizzbuzz(number) # return the output
-# end
+  range.each do |number|
+    output = ""
+
+    # check the number against each item in hash
+    hash.each do |key, value| 
+      output += value if number % key == 0 
+    end
+    
+    # if output is still blank, make output the number
+    output = number if output == ""
+
+    puts output
+
+  end
+end
