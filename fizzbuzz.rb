e6d449d9993a8622bf9fa3f21c7fb15aa210ae2f
JDtheGeek <john.dennehy@gmail.com>
2019-09-04 18:02:18 +0100

    Edited comments

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,18 @@
-
 def fizzbuzz
-  range = 1..30
-  hash = {
+  hash = { # numbers to divide by, and string to return - Add new key/values as required
     3 => "Fizz",
     5 => "Buzz"
   }
 
-  range.each do |number|
+  range = 1..30 # change range as required
+  range.each do |number| # loop over each number in the range
     output = ""
 
-    # check the number against each item in hash
-    hash.each do |key, value| 
+    hash.each do |key, value| # check the number against each item in hash
       output += value if number % key == 0 
     end
     
-    # if output is still blank, make output the number
-    output = number if output == ""
-
+    output = number if output == "" # if output is still blank, make output the number
     puts output
-
   end
 end
