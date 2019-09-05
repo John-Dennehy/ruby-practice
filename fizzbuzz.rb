54643962875538c04d51e3ac25edbe645a23e739
JDtheGeek <john.dennehy@gmail.com>
2019-09-05 10:53:51 +0100

    made range an optional parameter, defaults 30

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
-def fizzbuzz
+def fizzbuzz(count_to = 30)
   hash = { # numbers to divide by, and string to return - Add new key/values as required
     3 => "Fizz",
     5 => "Buzz"
   }
 
-  range = 1..30 # change range as required
-  range.each do |number| # loop over each number in the range
+  (1..count_to).each do |number| # loop over each number in the range
     output = ""
 
     hash.each do |key, value| # check the number against each item in hash
