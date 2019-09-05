4a8ca178f318e9582b9f82bb0a4a8fc7f228581d
JDtheGeek <john.dennehy@gmail.com>
2019-09-05 11:51:44 +0100

    Added comments to code

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@ def fizzbuzz(count_to = 30)
   }
 
   (1..count_to).each do |number| # loop over each number in the range
-    output = ""
+    output = "" # reset output at start of each loop
 
     hash.each do |key, value| # check the number against each item in hash
-      output += value if number % key == 0 
+      output += value if number % key == 0 # add value string to output if number is divisible by key
     end
     
     output = number if output == "" # if output is still blank, make output the number
