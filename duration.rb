c59c867cab183b25bb9f9662b1ec309cdb2c2f2f
JDtheGeek <john.dennehy@gmail.com>
2019-09-11 15:16:37 +0100

    refactoring code

--- duration.rb
+++ duration.rb
@@ -1,5 +1,4 @@
 def format_duration(seconds)
-  # your code here
   return "now" if seconds == 0
   unit_durations = duration_array(seconds)
   duration_string(unit_durations)
@@ -7,34 +6,28 @@ end
 
 def duration_array(seconds)
   units_of_time = [
-    {name: "year",   duration: 31536000, total: 0},
-    {name: "day",    duration: 86400,    total: 0},
-    {name: "hour",   duration: 3600,     total: 0},
-    {name: "minute", duration: 60,       total: 0},
-    {name: "second", duration: 1,        total: 0}
+    {name: "year",   duration: 31536000},
+    {name: "day",    duration: 86400},
+    {name: "hour",   duration: 3600},
+    {name: "minute", duration: 60},
+    {name: "second", duration: 1}
   ]
-
   array = []
   units_of_time.each do |unit|
-    if seconds >= unit[:duration]
-      unit[:total] = (seconds / unit[:duration])
-      seconds %= unit[:duration] # update seconds to equal the remaining seconds   
-    end
-    
     unit_name = unit[:name]
-    unit_name += "s" if unit[:total] > 1 # pluralise the unit if greater than 1
-    array << "#{unit[:total]} #{unit_name}" if unit[:total] > 0 # add total and unit to output array IF greater than 0
+    if seconds >= unit[:duration] 
+      unit_total = seconds / unit[:duration]
+      seconds %= unit[:duration] # update seconds to equal the remaining seconds 
+      unit_name += "s" if unit_total > 1 # pluralise the unit if greater than 1
+      array << "#{unit_total} #{unit_name}"  # add total and unit to output array IF greater than 0  
+    end
   end
-
   return array
 end
 
 def duration_string(array)
-  case
-  when array.length == 1
-    return array.first
-  when array.length > 1
-    last_join = " and " + array.pop
-    return array.join(", ") + last_join
-  end
+  return array.first if array.length == 1
+  # otherwise
+  last_join = " and " + array.pop
+  return array.join(", ") + last_join 
 end
